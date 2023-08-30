db7e9b0f0d10aaab8bcc2a3d0f727cc4e1ae6171
Talisson Coutinho <143600727+TalissonCdR@users.noreply.github.com>
2023-08-30 10:03:30 -0300

    Add files via upload

---
+++ Lista2/src/model/Locacao.java
@@ -0,0 +1,95 @@
+package model;
+
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Locacao {
+    private Integer id;
+    private LocalDateTime dataLocacao;
+    private LocalDateTime dataDevolucao;
+    private Integer quilometragem;
+    private double valorCalcao;
+    private double valorLocacao;
+    private Boolean devolvido;
+
+    public Locacao() {
+        super();
+    }
+
+    public Locacao(Integer id, LocalDateTime dataLocacao, LocalDateTime dataDevolucao, Integer quilometragem,
+                   Double valorCalcao, Double valorLocacao, Boolean devolvido) {
+        super();
+        this.id = id;
+        this.dataLocacao = dataLocacao;
+        this.dataDevolucao = dataDevolucao;
+        this.quilometragem = quilometragem;
+        this.valorCalcao = valorCalcao;
+        this.valorLocacao = valorLocacao;
+        this.devolvido = devolvido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataLocacao() {
+        return dataLocacao;
+    }
+
+    public void setDataLocacao(LocalDateTime dataLocacao) {
+        this.dataLocacao = dataLocacao;
+    }
+
+    public LocalDateTime getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(LocalDateTime dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Integer getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(Integer quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public Double getValorCalcao() {
+        return valorCalcao;
+    }
+
+    public void setValorCalcao(Double valorCalcao) {
+        this.valorCalcao = valorCalcao;
+    }
+
+    public Double getValorLocacao() {
+        return valorLocacao;
+    }
+
+    public void setValorLocacao(Double valorLocacao) {
+        this.valorLocacao = valorLocacao;
+    }
+
+    public Boolean isDevolvido() {
+        return devolvido;
+    }
+
+    public void setDevolvido(Boolean devolvido) {
+        this.devolvido = devolvido;
+    }
+
+    @Override
+    public String toString() {
+        return "\nLocacao [id=" + id + ", dataLocacao e horaLocacao=" + dataLocacao
+                + ", dataDevolucao e horaDevolucao=" + dataDevolucao + ", quilometragem="
+                + quilometragem + ", valorCalcao=" + NumberFormat.getCurrencyInstance().format(valorCalcao) + ", valorLocacao=" + NumberFormat.getCurrencyInstance().format(valorLocacao) + ", devolvido="
+                + devolvido + "]";
+    }
+}
